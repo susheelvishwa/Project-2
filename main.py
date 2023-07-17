624a4e05e3f7d5f6e85ab831028fa067e697d281
Susheel Vishwakarma <susheelvishwakarma.india@gmail.com>
2023-07-18 00:24:17 +0530

    Nothing changes

--- main.py
+++ main.py
@@ -5,6 +5,4 @@ obj = canvas.sketch_from_image(
     path="C:\\Users\\sushe\\OneDrive\\Documents\\project\\sketch\\susheel Vishwakarma .jpg",
     save=False,
 )
-# obj.load_svg(file="data.npy")
 obj.draw(threshold=50)
-# obj = lib.rdj()
